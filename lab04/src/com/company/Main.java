f72dd79a02b029f962e91318789917488df6ae9e
TimofeevMikhail <31990242+TimofeevMikhail@users.noreply.github.com>
2017-11-18 01:15:01 +0700

    Stream

---
+++ lab04/src/com/company/Main.java
@@ -0,0 +1,17 @@
+﻿package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        FileAnalizer customAnalizer = new FileAnalizer("input.txt");
+
+        System.out.println("Количество букв: " + customAnalizer.countLetters());
+        System.out.println("Количество слов: " + customAnalizer.countWords());
+       System.out.println("Количество строк: " + customAnalizer.countLines());
+       System.out.println("Частотный анализ: " + customAnalizer.countFrequencyCharacteristic());
+
+
+
+    }
+}
