09e84f6e81606db4b9e9ff79844f40c253a7b53d
TimofeevMikhail <31990242+TimofeevMikhail@users.noreply.github.com>
2017-12-02 06:34:14 +0000

    LockerLockedException

--- lab08/src/LockerLockedException.java
+++ lab08/src/LockerLockedException.java
@@ -1,4 +1,6 @@
-public class LockerLockedException extends Exception {
+import java.rmi.AccessException;
+
+public class LockerLockedException extends AccessException  {
 
     public LockerLockedException (){
         super("Locker is locked");
